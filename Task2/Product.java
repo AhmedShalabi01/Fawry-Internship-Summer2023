e1ab359f1b495c38d1d847b10d844da33089a320
AhmedShalabi01 <119356507+AhmedShalabi01@users.noreply.github.com>
2023-08-06 20:47:07 +0300

    Update Product.java

--- Task2/Product.java
+++ Task2/Product.java
@@ -2,9 +2,9 @@ public class Product
 {
     private final String productName;
     private final double productPrice;
-    public Product(String productN,double productP ) {
-        this.productName = productN;
-        this.productPrice = productP;
+    public Product(String productName,double productPrice ) {
+        this.productName = productName;
+        this.productPrice = productPrice;
     }
     public String getProductName() {
         return productName;
