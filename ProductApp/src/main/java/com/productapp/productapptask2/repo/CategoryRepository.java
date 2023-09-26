408af1658fed4c502266c17293f2f7d14536c4ff
AhmedShalabi01 <119356507+AhmedShalabi01@users.noreply.github.com>
2023-09-26 15:16:32 +0300

    Delete ProductApp directory

--- ProductApp/src/main/java/com/productapp/productapptask2/repo/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.productapp.productapptask2.repo;
-
-import com.productapp.productapptask2.entity.Category;
-import com.productapp.productapptask2.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-}
